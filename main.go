877496e4c3e5a5d5184ba27b17036ddfe86500a2
fianine <princealfiyana@gmail.com>
2020-12-03 01:00:12 +0700

    Go Mod

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 	"log"
 	"net/http"
 
+	_ "github.com/go-project/models/model"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
-
-	_ "github.com/go-project/models/model"
 )
 
 func main() {
